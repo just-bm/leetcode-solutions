e6db250ed7aaed0134d4fa88c315c0c6b7c3e8ff
Balamurugan T <159176391+just-bm@users.noreply.github.com>
2025-02-03 21:49:04 +0530

    Create power_of_two.py

---
+++ power_of_two.py
@@ -0,0 +1,10 @@
+#231 leetcode python solution
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n==0:
+            return False
+        return (n & (n-1))==0
+
+
+        
